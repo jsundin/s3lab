9719cd106e45c82381d37b4bf67d68bac6c2eba3
Johan Sundin <johan.p.sundin@lm.se>
2017-09-25 08:55:09 +0200

    + saker

--- src/main/java/s4lab/db/FileRepository.java
+++ src/main/java/s4lab/db/FileRepository.java
@@ -19,26 +19,6 @@ public class FileRepository {
     this.dbHandler = dbHandler;
   }
 
-  public int deleteAndInsertFiles(List<File> files) throws SQLException {
-    int inserts = 0;
-    int n = 0;
-    try (Connection conn = dbHandler.getConnection()) {
-      try (PreparedStatement stmt = conn.prepareStatement("delete from file")) {
-        stmt.executeUpdate();
-      }
-      try (PreparedStatement stmt = conn.prepareStatement("insert into file(id, filename) values(?, ?)")) {
-        for (File file : files) {
-          stmt.setString(1, UUID.randomUUID().toString());
-          stmt.setString(2, file.toString());
-          inserts += stmt.executeUpdate();
-          n++;
-          if (n % 100 == 0) System.out.println(n + "/" + files.size());
-        }
-      }
-    }
-    return inserts;
-  }
-
   public void saveSimple(File file) throws SQLException {
     try (Connection conn = dbHandler.getConnection()) {
       String id = UUID.randomUUID().toString();
